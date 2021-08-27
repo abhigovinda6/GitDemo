e781c04834ffc2c3bb9d023937e023a26564e888
Abhishek <abhigovinda6@gmail.com>
2021-08-27 11:02:15 +0530

    Intial push to git repository

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+
+	
+	@Test
+	public void postnativeAppAndroid()
+	{
+		System.out.println("Native App Android");
+	}
+	
+	@Test
+	public void postIOSApps()
+	{
+		System.out.println("IOSApps");
+
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("Browser Automation");
+	}
+	
+	@Test
+	public void elementUI()
+	{
+		System.out.println("elementUI");
+
+	}
+
+}
